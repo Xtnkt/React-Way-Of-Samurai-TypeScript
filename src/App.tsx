88456403316ca9abfb6aedb387f14c4aad59d8e6
Vladislav Karas <dncmorgan@yandex.ru>
2022-09-01 18:36:38 +0300

    1-9 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Components/Header/Header";
+import {Technologies} from "./Components/Technologies/Technologies";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Header(props: any) {
+    return (
+        <div>
+            <a href='#s'>Home</a>
+            <a href='#s'>News Feed</a>
+            <a href='#s'>Messages</a>
+        </div>
+    )
+}
+
